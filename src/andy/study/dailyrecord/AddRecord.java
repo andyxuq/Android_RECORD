4831d6d08e58a74554b76155174787996f5588f6
andyxuq <andyxuq@gmail.com>
2014-06-23 23:01:46 +0800

    -fouth commit

--- src/andy/study/dailyrecord/AddRecord.java
+++ src/andy/study/dailyrecord/AddRecord.java
@@ -45,9 +45,6 @@ public class AddRecord extends Activity{
 	private Button showRecord_button;
 	private EditText costText;
 	private EditText noteText;
-	
-	
-	
 	private TextView suggestView;
 	
 	private TextView showDateView;
